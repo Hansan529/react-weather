62a846124b072f1ce88dc53ac2150f1e8cf1d593
Hansan529 <hansan0529@gmail.com>
2023-05-28 13:41:32 +0000

    docs: 영화 목록 -> 기상 날씨로 변경
    영화 포스터를 가져올 API가 지원 종료되어 얻을 수 없어서 대체

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,21 +1,54 @@
 import { useState, useEffect } from "react";
 import Movie from "../components/Movie";
 import dateString from "../Date";
+import axios from "axios";
 
 function Home() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
+  const [time, setTime] = useState("0600");
+  const [weather, setWeather] = useState([]);
 
   const getMovies = async () => {
-    const json = await (
-      await fetch(
-        `https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=${
-          dateString - 1
-        }`
-      )
-    ).json();
-    setMovies(json.boxOfficeResult.dailyBoxOfficeList);
-    setLoading(false);
+    // const json = await (
+    //   await fetch(
+    //     `https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=${
+    //       dateString - 1
+    //     }`
+    //   )
+    // ).json();
+    // const result = json.boxOfficeResult.dailyBoxOfficeList;
+    // setMovies(result);
+
+    const url = `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst`;
+    const options = {
+      serviceKey: process.env.REACT_APP_SERVICEKEY,
+      pageNo: 1,
+      numOfRows: 1000,
+      dataType: "JSON",
+      base_date: dateString,
+      base_time: time,
+      nx: 55,
+      ny: 127,
+    };
+    try {
+      const resultUrl = decodeURIComponent(
+        new URLSearchParams(options).toString()
+      );
+      const response = await axios.get(`${url}?${resultUrl}`);
+      const {
+        response: {
+          body: {
+            items: { item },
+          },
+        },
+      } = await response.data;
+      console.log(item);
+      setWeather(item);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
   };
   useEffect(() => {
     getMovies();
@@ -28,7 +61,8 @@ function Home() {
         <h1>로딩 중...</h1>
       ) : (
         <div>
-          {movies.map((movie) => (
+          {weather[3].obsrValue}
+          {/* {movies.map((movie, index) => (
             <Movie
               key={movie.movieCd}
               id={movie.movieCd}
@@ -38,7 +72,7 @@ function Home() {
               audiCnt={parseInt(movie.audiCnt)}
               audiAcc={parseInt(movie.audiAcc)}
             />
-          ))}
+          ))} */}
         </div>
       )}
     </div>
