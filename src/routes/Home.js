f3db573237d2212cb6402c5a2e1a4b4e939a0b1a
Hansan529 <hansan0529@gmail.com>
2023-05-28 14:25:46 +0000

    feat: 시간이 변경되면 함수 재시작

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import Movie from "../components/Movie";
 import dateString from "../Date";
 import axios from "axios";
+import Weather from "../components/Weather";
 
 function Home() {
   const [loading, setLoading] = useState(true);
@@ -9,17 +10,11 @@ function Home() {
   const [time, setTime] = useState("0600");
   const [weather, setWeather] = useState([]);
 
-  const getMovies = async () => {
-    // const json = await (
-    //   await fetch(
-    //     `https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=${
-    //       dateString - 1
-    //     }`
-    //   )
-    // ).json();
-    // const result = json.boxOfficeResult.dailyBoxOfficeList;
-    // setMovies(result);
+  const onChange = async (e) => {
+    setTime(e.currentTarget.value);
+  };
 
+  const getMovies = async () => {
     const url = `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst`;
     const options = {
       serviceKey: process.env.REACT_APP_SERVICEKEY,
@@ -43,7 +38,6 @@ function Home() {
           },
         },
       } = await response.data;
-      console.log(item);
       setWeather(item);
       setLoading(false);
     } catch (error) {
@@ -52,27 +46,33 @@ function Home() {
   };
   useEffect(() => {
     getMovies();
-  }, []);
+  }, [time]);
 
   return (
     <div>
-      <h1>최신 영화</h1>
+      <select onChange={onChange}>
+        <option value="0600">AM 06:00</option>
+        <option value="0700">AM 07:00</option>
+        <option value="0800">AM 08:00</option>
+        <option value="0900">AM 09:00</option>
+        <option value="1000">AM 10:00</option>
+        <option value="1100">AM 11:00</option>
+        <option value="1200">AM 12:00</option>
+      </select>
+      <h1>기상 상황</h1>
       {loading ? (
         <h1>로딩 중...</h1>
       ) : (
         <div>
-          {weather[3].obsrValue}
-          {/* {movies.map((movie, index) => (
-            <Movie
-              key={movie.movieCd}
-              id={movie.movieCd}
-              rank={parseInt(movie.rank)}
-              movieNm={movie.movieNm}
-              openDt={parseInt(movie.openDt)}
-              audiCnt={parseInt(movie.audiCnt)}
-              audiAcc={parseInt(movie.audiAcc)}
+          {weather.map((arr, index) => (
+            <Weather
+              key={index}
+              baseDate={arr.baseDate}
+              baseTime={arr.baseTime}
+              category={arr.category}
+              obsrValue={arr.obsrValue}
             />
-          ))} */}
+          ))}
         </div>
       )}
     </div>
