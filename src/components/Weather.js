80b90b9fab7b69b3e12dc4c6a1a7568e1e807ed0
Hansan529 <hansan0529@gmail.com>
2023-05-29 01:07:20 +0000

    docs: 계산된 VEC 사용으로 변경, obsrValue 숫자로 변경

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -5,27 +5,25 @@ function Weather({ baseDate, baseTime, category, obsrValue }) {
   let info;
   let result;
   let icon;
-  const A =
-    Math.atan2(Number(obsrValue[1]), Number(obsrValue[0])) * (180 / Math.PI);
-  const B = (A + 360) % 360;
-  console.log("B: ", B);
+  obsrValue = Number(obsrValue);
+
   switch (category) {
     case "PTY":
       info = "현재 날씨";
       switch (obsrValue) {
-        case "0":
+        case 0:
           result = "맑음";
           icon = "clear";
           break;
-        case "1":
+        case 1:
           result = "비";
           icon = "rain";
           break;
-        case "2":
+        case 2:
           result = "비/눈";
           icon = "rain-snow";
           break;
-        case "3":
+        case 3:
           result = "눈";
           icon = "snow";
           break;
@@ -39,57 +37,49 @@ function Weather({ baseDate, baseTime, category, obsrValue }) {
       break;
     case "RN1":
       info = "1시간 강수량";
-      result = Number(obsrValue) !== 0 ? obsrValue + "mm" : "-";
+      result = obsrValue !== 0 ? obsrValue + "mm" : "-";
       break;
     case "T1H":
       info = "현재 기온";
       result = obsrValue + "℃";
       break;
-    case "UUU":
-      // info = "동서바람성분";
-      break;
     case "VEC":
       info = "풍향";
-      result = obsrValue + "°";
-      break;
-    case "VVV":
-      // info = "남북바람성분";
-      break;
-    case "WSD":
-      info = "풍속";
-      break;
-    case "WDN":
-      info = "방향";
-
+      result = obsrValue;
       switch (true) {
-        case obsrValue[0] === String(0) && obsrValue[0] > 0:
+        case obsrValue === 0:
           result = "북풍";
           break;
-        case obsrValue[0] > 0 && obsrValue[0] < 0:
+        case obsrValue > 0 && obsrValue < 90:
           result = "북동풍";
           break;
-        case obsrValue[0] > 0 && obsrValue[0] === String(0):
+        case obsrValue === 90:
           result = "동풍";
           break;
-        case obsrValue[0] > 0 && obsrValue[1] > 0:
+        case obsrValue > 90 && obsrValue < 180:
           result = "남동풍";
           break;
-        case obsrValue[0] === "0" && obsrValue[0] < 0:
+        case obsrValue === 180:
           result = "남풍";
           break;
-        case obsrValue[0] < 0 && obsrValue[0] > 0:
+        case obsrValue > 180 && obsrValue < 270:
           result = "남서풍";
           break;
-        case obsrValue[0] < 0 && obsrValue[0] === "0":
+        case obsrValue === 270:
           result = "서풍";
           break;
-        case obsrValue[0] < 0 && obsrValue[0] < 0:
+        case obsrValue > 270 && obsrValue < 360:
           result = "북서풍";
           break;
         default:
           result = "-";
           break;
       }
+      break;
+    case "WSD":
+      info = "바람";
+      result = obsrValue + "m/s";
+      break;
 
     default:
       break;
@@ -97,7 +87,7 @@ function Weather({ baseDate, baseTime, category, obsrValue }) {
   return (
     <li>
       <p>{info}</p>
-      {result} {obsrValue} <i className={icon}></i>
+      {result} <i className={icon}></i>
     </li>
   );
 }
