ddc0005eb9708caa346b1a7e7db1851d55f68156
Hansan529 <hansan0529@gmail.com>
2023-05-29 03:50:21 +0000

    update: Header 컴포넌트 생성

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import { dateTime, dateString } from "../Date";
+import styles from "../styles/components/Header.module.css";
+
+function Header() {
+  const date = new Date();
+  return (
+    <header>
+      <div>
+        <h1 className={styles.logo}>로고 자리</h1>
+        <nav>
+          <ul>
+            <li>
+              {date.getMonth() + 1}월 {date.getDate()}일
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div>
+        <p>날씨</p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
