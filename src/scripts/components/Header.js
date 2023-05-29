395a36b8625472adeb1085a278b57d33b333f8e8
Hansan529 <hansan0529@gmail.com>
2023-05-29 16:09:26 +0000

    update: 사용자 정의 요소 추가

--- src/scripts/components/Header.js
+++ src/scripts/components/Header.js
@@ -1,23 +1,40 @@
 import { dateTime, dateString } from "../config/Date";
 import styles from "../../styles/components/Header.module.css";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function Header() {
+  const [location, setLocation] = useState([]);
   const date = new Date();
+
   return (
     <header>
-      <div>
-        <h1 className={styles.logo}>로고 자리</h1>
+      <center-component>
+        <h1 className={styles.logo__Wrap}>
+          <Link to="/">
+            <img
+              src={`${process.env.PUBLIC_URL}/images/components/logo256.png`}
+              alt="logo"
+              className={styles.logo}
+            />
+            <small>기상청 단기예보</small>
+          </Link>
+        </h1>
         <nav>
           <ul>
             <li>
-              {date.getMonth() + 1}월 {date.getDate()}일
+              <Link to="/">초단기실황조회</Link>
             </li>
           </ul>
         </nav>
-      </div>
-      <div>
-        <p>날씨</p>
-      </div>
+        <ul className="toDay">
+          <li>
+            <p>
+              {date.getMonth() + 1}월 {date.getDate()}일
+            </p>
+          </li>
+        </ul>
+      </center-component>
     </header>
   );
 }
