573e2fe029ef0a2ed99d1977d0644a84bdf96e50
Hansan529 <hansan0529@gmail.com>
2023-05-30 01:04:59 +0000

    update: 위경도 -> 격자좌표 변환값으로 API 요청

--- src/scripts/components/Weather.js
+++ src/scripts/components/Weather.js
@@ -6,7 +6,7 @@ import axios from "axios";
 
 //* Component Import
 import WeatherScreen from "../screens/WeatherScreen";
-import { currentLocation } from "../config/Geolocation";
+import { currentLocation, coordinateToGrid } from "../config/Geolocation";
 
 //* Style Import
 import "../../styles/components/Weather.css";
@@ -18,9 +18,10 @@ function Weather() {
   const [time, setTime] = useState(
     `${(dateTime.substring(0, 2) - 1).toString().padStart(2, "0")}00`
   );
-  const [weather, setWeather] = useState([]);
+  const [ultraSrtNcst, setUltraSrtNcst] = useState([]);
+  const [ultraSrtFcst, setUltraSrtFcst] = useState([]);
   const [disabled, setDisabled] = useState(false);
-  const [coordinate, setCoordinate] = useState([]);
+  const [coordinate, setCoordinate] = useState([55, 127]);
 
   // 24번 option 생성하기 위한 가상 배열 생성
   const length = 24;
@@ -28,10 +29,14 @@ function Weather() {
 
   // 위치 가져오기 성공
   const getCurrentPosition = async (locationData) => {
-    await setCoordinate([
-      parseInt(locationData.latitude),
-      parseInt(locationData.longitude),
-    ]);
+    // 기상청이 사용하는 격자 X, Y로 변환하기
+    const result = coordinateToGrid(
+      "toXY",
+      locationData.latitude,
+      locationData.longitude
+    );
+    console.log("result: ", result);
+    await setCoordinate([result.x, result.y]);
   };
 
   // 위치 가져오기 실패
@@ -44,29 +49,25 @@ function Weather() {
     currentLocation(getCurrentPosition, failCurrentPosition);
   }, []);
 
-  const getWeather = useCallback(async () => {
+  // API 파라미터 값
+  const options = {
+    serviceKey: process.env.REACT_APP_SERVICEKEY,
+    pageNo: 1,
+    numOfRows: 1000,
+    dataType: "JSON",
+    base_date:
+      Number(dateTime) > 240000
+        ? (Number(dateString) - 1).toString()
+        : dateString,
+    base_time: time,
+    nx: coordinate[0],
+    ny: coordinate[1],
+  };
+
+  // ^ 초단기실황조회
+  const getUltraSrtNcst = useCallback(async () => {
     // API 경로
-    const url = `https://apiServer.hxan.net/api/weather/`;
-    // API 파라미터 값
-    const options = {
-      serviceKey: process.env.REACT_APP_SERVICEKEY,
-      pageNo: 1,
-      numOfRows: 1000,
-      dataType: "JSON",
-      base_date:
-        Number(dateTime) > 240000
-          ? (Number(dateString) - 1).toString()
-          : dateString,
-      base_time: time,
-      nx:
-        Array.isArray(coordinate) && coordinate.length === 0
-          ? 37
-          : coordinate[0],
-      ny:
-        Array.isArray(coordinate) && coordinate.length === 0
-          ? 127
-          : coordinate[1],
-    };
+    const url = `https://apiServer.hxan.net/api/weather/getUltraSrtNcst/`;
     try {
       // 객체 목록을 파라미터로 변경
       const resultUrl = decodeURIComponent(
@@ -83,11 +84,44 @@ function Weather() {
         },
       } = await response.data;
       // 날씨 설정
-      setWeather(
+      setUltraSrtNcst(
         item.filter(
           (item) => item.category !== "UUU" && item.category !== "VVV"
         )
       );
+      // 로딩 종료
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
+  }, [time, coordinate]);
+
+  // ^ 초단기예보조회
+  const getUltraSrtFcst = useCallback(async () => {
+    // API 경로
+    const url = `https://apiServer.hxan.net/api/weather/getUltraSrtFcst/`;
+    try {
+      // 객체 목록을 파라미터로 변경
+      const resultUrl = decodeURIComponent(
+        new URLSearchParams(options).toString()
+      );
+      // API 요청
+      const response = await axios.get(`${url}${resultUrl}`);
+      // API 데이터 콜백
+      const {
+        response: {
+          body: {
+            items: { item },
+          },
+        },
+      } = await response.data;
+      console.log("item: ", item);
+      // 날씨 설정
+      // setUltraSrtNcst(
+      //   item.filter(
+      //     (item) => item.category !== "UUU" && item.category !== "VVV"
+      //   )
+      // );
       // 로딩 종료
       setLoading(false);
     } catch (error) {
@@ -104,10 +138,14 @@ function Weather() {
     }, 1000);
   };
 
-  // ^ 최초 렌더링, 시간과 위치 값이 변하면 재실행
+  // ^ 초단기실황조회 최초 렌더링, 시간과 위치 값이 변하면 재실행
   useEffect(() => {
-    getWeather();
-  }, [time, coordinate]);
+    getUltraSrtNcst();
+  }, [time, getUltraSrtNcst, coordinate]);
+
+  // useEffect(() => {
+  //   getUltraSrtFcst();
+  // }, []);
 
   // ! 렌더링
   return (
@@ -117,6 +155,31 @@ function Weather() {
       ) : (
         <center-component>
           <h2>초단기실황조회</h2>
+          {/* 
+          초단기실황
+          매시간 30분에 생성되고 10분마다 최신 정보로 업데이트
+          기준 시간: 00시 
+          생성시간: 00:30 
+          Base_time: 0000 
+          API제공시간(~이후): 00:40 
+          */}
+          {/* 
+          초단기예보
+          매시간 30분에 생성되고 10분마다 최신 정보로 업데이트(기온, 습도, 바람)
+          기준 시간: 00시
+          생성시각: 00:30
+          Base_time: 0030
+          API제공시간(~이후): 00:45
+          예보시간 (매 발표시각마다 6시간 예보) 
+          ㄴ h시~h+1시: 0~1시
+          ㄴ h+1시~h+2시: 1~시
+          */}
+          {/* 
+          단기예보
+          Base_time: 0200, 0500, 0800, 1100, 1400, 1700, 2000, 2300 (1일 8회)
+          API제공시간(~이후): 02:10, 05:10, 08:10, 11:10, 14:10, 17:10, 20:10, 23:10
+          */}
+          <abbr>매시간 30분에 생성되고 10분마다 최선 정보로 업데이트</abbr>
           <div className={styles.timeSelect}>
             <p>Time</p>
             <select onChange={onChange} disabled={disabled} value={time}>
@@ -134,7 +197,7 @@ function Weather() {
             </select>
           </div>
           <weather-component>
-            {weather.map((arr, index) => (
+            {ultraSrtNcst.map((arr, index) => (
               <WeatherScreen
                 key={index}
                 baseDate={arr.baseDate}
