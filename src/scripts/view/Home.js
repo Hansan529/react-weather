00cb126a6beb910c9629642da76fbc219a95f87e
Hansan529 <hansan0529@gmail.com>
2023-05-31 08:37:08 +0000

    update: Footer 추가

--- src/scripts/view/Home.js
+++ src/scripts/view/Home.js
@@ -3,12 +3,14 @@ import "../../styles/styles.css";
 // Component Import
 import Header from "../components/Header";
 import Weather from "../components/Weather.js";
+import Footer from "../components/Footer";
 
 function Home() {
   return (
     <>
       <Header />
       <Weather />
+      <Footer />
     </>
   );
 }
