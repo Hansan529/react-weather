475c2743963b6693597a99ec2a547ea4a1271c92
Hansan529 <hansan0529@gmail.com>
2023-05-31 07:14:33 +0000

    feat: 위치 주소 변환해서 위치 가져오기

--- src/scripts/components/Header.js
+++ src/scripts/components/Header.js
@@ -2,10 +2,54 @@ import { dateTime, dateString } from "../config/Date";
 import styles from "../../styles/components/Header.module.css";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { coordinateToGrid, currentLocation } from "../config/Geolocation";
+import axios from "axios";
 
 function Header() {
-  const [location, setLocation] = useState([]);
+  const [locationCoordinate, setLocationCoordinate] = useState([
+    37.5642135, 127.0016985,
+  ]);
+  const [showAddress, setAddress] = useState("");
+  const [addressLoad, setAddressLoad] = useState(false);
   const date = new Date();
+
+  // 위치 가져오기 성공
+  const getCurrentPosition = async (locationData) => {
+    // 기상청이 사용하는 격자 X, Y로 변환하기
+    setLocationCoordinate([locationData.latitude, locationData.longitude]);
+    // 해당 경위도를 갖고 검색 파라미터로 변경
+    const params = decodeURIComponent(
+      new URLSearchParams({
+        x: locationData.longitude,
+        y: locationData.latitude,
+        input_coord: "WGS84",
+      }).toString()
+    );
+    // API 요청
+    const { documents } = await (
+      await axios.get(
+        `https://apiServer.hxan.net/api/coordinate/coord2Address/${params}`,
+        {
+          headers: {
+            Authorization: `KakaoAK ${process.env.REACT_APP_KAKAO_REST_API_KEY}`,
+          },
+        }
+      )
+    ).data;
+    const { address } = documents[0];
+    setAddress(address);
+    setAddressLoad(true);
+  };
+
+  // 위치 가져오기 실패
+  const failCurrentPosition = (error) => {
+    console.log(error);
+  };
+
+  // ^ 비연속적 렌더링 방식
+  useEffect(() => {
+    currentLocation(getCurrentPosition, failCurrentPosition);
+  }, []);
 
   return (
     <header>
@@ -20,13 +64,13 @@ function Header() {
             <small>기상청 단기예보</small>
           </Link>
         </h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">초단기실황조회</Link>
-            </li>
-          </ul>
-        </nav>
+        <div>
+          <p>
+            {addressLoad
+              ? `${showAddress.region_1depth_name} ${showAddress.region_2depth_name}`
+              : "서울"}
+          </p>
+        </div>
         <ul className="toDay">
           <li>
             <p>
