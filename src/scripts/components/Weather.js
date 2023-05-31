734ad73aeb2bc090e9d5c7f4b10a65f3efc54a7c
Hansan529 <hansan0529@gmail.com>
2023-05-31 07:13:53 +0000

    feat: 초단기예측실황 조회, 현위치를 기준으로 조회, 예보 Toggle 형식
    function Name 간결화
    위치 정보를 거부할 경우 기본값 서울 좌표로 설정

--- src/scripts/components/Weather.js
+++ src/scripts/components/Weather.js
@@ -14,12 +14,16 @@ import styles from "../../styles/components/Weather.module.css";
 
 //^ Component
 function Weather() {
-  const [loading, setLoading] = useState(true);
-  const [ultraSrtNcstTime, setUltraSrtNcstTime] = useState(
+  const [ncstTime, setNcstTime] = useState(
     `${(dateTime.substring(0, 2) - 1).toString().padStart(2, "0")}00`
   );
-  const [ultraSrtNcst, setUltraSrtNcst] = useState([]);
-  const [ultraSrtFcst, setUltraSrtFcst] = useState([]);
+  const [fcstTime, setFcstTime] = useState(
+    `${(dateTime.substring(0, 2) - 1).toString().padStart(2, "0")}30`
+  );
+  const [ncst, setNcst] = useState([]);
+  const [ncstLoad, setNcstLoad] = useState("");
+  const [fcst, setFcst] = useState([]);
+  const [fcstLoad, setFcstLoad] = useState("");
   const [disabled, setDisabled] = useState(false);
   const [coordinate, setCoordinate] = useState([55, 127]);
 
@@ -27,31 +31,10 @@ function Weather() {
   const length = 24;
   const selectTime = Array.from({ length }, (_, index) => index);
 
-  // 위치 가져오기 성공
-  const getCurrentPosition = async (locationData) => {
-    // 기상청이 사용하는 격자 X, Y로 변환하기
-    const result = coordinateToGrid(
-      "toXY",
-      locationData.latitude,
-      locationData.longitude
-    );
-    await setCoordinate([result.x, result.y]);
-  };
-
-  // 위치 가져오기 실패
-  const failCurrentPosition = (error) => {
-    console.log(error);
-  };
-
-  // 위치 정보 가져오기
-  useEffect(() => {
-    currentLocation(getCurrentPosition, failCurrentPosition);
-  }, []);
-
   // ^ 초단기실황조회
-  const getUltraSrtNcst = useCallback(async () => {
+  const weatherNcst = useCallback(async () => {
     // API 경로
-    const url = `https://apiServer.hxan.net/api/weather/getUltraSrtNcst/`;
+    const url = `https://apiServer.hxan.net/api/weather/ncst/`;
     // API 파라미터 값
     const options = {
       serviceKey: process.env.REACT_APP_SERVICEKEY,
@@ -62,7 +45,7 @@ function Weather() {
         Number(dateTime) > 240000
           ? (Number(dateString) - 1).toString()
           : dateString,
-      base_time: ultraSrtNcstTime,
+      base_time: ncstTime,
       nx: coordinate[0],
       ny: coordinate[1],
     };
@@ -83,41 +66,131 @@ function Weather() {
         },
       } = await response.data;
       // 날씨 설정
-      setUltraSrtNcst(
+      setNcst(
         item.filter(
           (item) => item.category !== "UUU" && item.category !== "VVV"
         )
       );
-      // 로딩 종료
-      setLoading(false);
     } catch (error) {
       console.error(error);
     }
-  }, [ultraSrtNcstTime, coordinate]);
+  }, [ncstTime, coordinate]);
 
-  // ^Select의 값이 변하면 time 변경, 1초동안 select 비활성화
-  const onChange = (e) => {
+  // ^ 초단기예측실황
+  const weatherFcst = useCallback(async () => {
+    // API 경로
+    const url = `https://apiServer.hxan.net/api/weather/fcst/`;
+    // API 파라미터 값
+    const options = {
+      serviceKey: process.env.REACT_APP_SERVICEKEY,
+      pageNo: 1,
+      numOfRows: 1000,
+      dataType: "JSON",
+      base_date:
+        Number(dateTime) > 240000
+          ? (Number(dateString) - 1).toString()
+          : dateString,
+      base_time: fcstTime,
+      nx: coordinate[0],
+      ny: coordinate[1],
+    };
+
+    try {
+      // 객체 목록을 파라미터로 변경
+      const resultUrl = decodeURIComponent(
+        new URLSearchParams(options).toString()
+      );
+      // API 요청
+      const response = await axios.get(url + resultUrl);
+      // API 데이터 콜백
+      const {
+        response: {
+          body: {
+            items: { item },
+          },
+        },
+      } = await response.data;
+      // 날씨 설정
+      setFcst(item);
+      // 로딩 종료
+    } catch (error) {
+      console.error(error);
+    }
+  }, [fcstTime, coordinate]);
+  // ^ 함수
+  // 비활성화 함수
+  const disabledFnc = () => {
     setDisabled(true);
-    setUltraSrtNcstTime(e.currentTarget.value);
     setTimeout(() => {
       setDisabled(false);
     }, 1000);
   };
 
-  // ^ 초단기실황조회 최초 렌더링, 시간과 위치 값이 변하면 재실행
+  // 위치 가져오기 성공
+  const getCurrentPosition = async (locationData) => {
+    // 기상청이 사용하는 격자 X, Y로 변환하기
+    const result = coordinateToGrid(
+      "toXY",
+      locationData.latitude,
+      locationData.longitude
+    );
+    await setCoordinate([result.x, result.y]);
+  };
+
+  // 위치 가져오기 실패
+  const failCurrentPosition = (error) => {
+    console.log(error);
+    setCoordinate("위치를 찾을 수 없음");
+  };
+
+  // ^ 이벤트 함수
+  // 초단기실황 시간 변화
+  const onChangeNcstTime = (e) => {
+    disabledFnc();
+    setNcstTime(e.currentTarget.value);
+  };
+  // 초단기실황 Show/Hide
+  const onClickNcst = () => {
+    setNcstLoad(ncstLoad ? false : true);
+  };
+
+  // 초단기예보 시간 변화
+  const onChangeFcstTime = (e) => {
+    disabledFnc();
+    setFcstTime(e.currentTarget.value);
+  };
+  // 초단기예보 Show/Hide
+  const onClickFcst = () => {
+    setFcstLoad(fcstLoad ? false : true);
+  };
+
+  // ^ 비연속적 렌더링 방식
+  // 위치 정보 가져오기
   useEffect(() => {
-    getUltraSrtNcst();
-  }, [ultraSrtNcstTime, getUltraSrtNcst, coordinate]);
+    currentLocation(getCurrentPosition, failCurrentPosition);
+  }, []);
+
+  // 초단기실황
+  useEffect(() => {
+    weatherNcst();
+  }, [ncstTime, weatherNcst, coordinate]);
+
+  // 초단기예보
+  useEffect(() => {
+    weatherFcst();
+  }, [fcstTime, weatherFcst, coordinate]);
 
   // ! 렌더링
   return (
     <main>
-      {loading ? (
-        <h2>로딩 중</h2>
-      ) : (
-        <center-component>
-          <h2>초단기실황조회</h2>
-          {/* 
+      <center-component>
+        <h2
+          onClick={onClickNcst}
+          title="30분에 생성되고, 10분마다 정보 업데이트"
+        >
+          초단기실황조회
+        </h2>
+        {/* 
           초단기실황
           매시간 30분에 생성되고 10분마다 최신 정보로 업데이트
           기준 시간: 00시 
@@ -125,7 +198,53 @@ function Weather() {
           Base_time: 0000 
           API제공시간(~이후): 00:40 
           */}
-          {/* 
+        {/* 
+          단기예보
+          Base_time: 0200, 0500, 0800, 1100, 1400, 1700, 2000, 2300 (1일 8회)
+          API제공시간(~이후): 02:10, 05:10, 08:10, 11:10, 14:10, 17:10, 20:10, 23:10
+          */}
+        {ncstLoad ? (
+          <div className="hiddenContent">
+            <div className={styles.timeSelect}>
+              <p>Time</p>
+              <select
+                onChange={onChangeNcstTime}
+                disabled={disabled}
+                value={ncstTime}
+              >
+                {selectTime.map((arr, index) => {
+                  return (
+                    <option
+                      key={index}
+                      value={`${arr.toString().padStart(2, "0")}00`}
+                      disabled={dateTime.substring(0, 2) - 1 < index}
+                    >
+                      {arr.toString().padStart(2, "0")}시
+                    </option>
+                  );
+                })}
+              </select>
+            </div>
+            <weather-component>
+              {ncst.map((arr, index) => (
+                <WeatherScreen
+                  key={index}
+                  baseDate={arr.baseDate}
+                  baseTime={arr.baseTime}
+                  category={arr.category}
+                  value={arr.obsrValue}
+                />
+              ))}
+            </weather-component>
+          </div>
+        ) : null}
+        <h2
+          onClick={onClickFcst}
+          title="30분에 생성되고 10분마다 정보 업데이트(기온, 습도, 바람)"
+        >
+          초단기예보조회
+        </h2>
+        {/* 
           초단기예보
           매시간 30분에 생성되고 10분마다 최신 정보로 업데이트(기온, 습도, 바람)
           기준 시간: 00시
@@ -136,76 +255,31 @@ function Weather() {
           ㄴ h시~h+1시: 0~1시
           ㄴ h+1시~h+2시: 1~시
           */}
-          {/* 
-          단기예보
-          Base_time: 0200, 0500, 0800, 1100, 1400, 1700, 2000, 2300 (1일 8회)
-          API제공시간(~이후): 02:10, 05:10, 08:10, 11:10, 14:10, 17:10, 20:10, 23:10
-          */}
-          <abbr>매시간 30분에 생성되고 10분마다 최선 정보로 업데이트</abbr>
-          <div className={styles.timeSelect}>
-            <p>Time</p>
-            <select
-              onChange={onChange}
-              disabled={disabled}
-              value={ultraSrtNcstTime}
-            >
-              {selectTime.map((arr, index) => {
-                return (
-                  <option
-                    key={index}
-                    value={`${arr.toString().padStart(2, "0")}00`}
-                    disabled={dateTime.substring(0, 2) - 1 < index}
-                  >
-                    {arr.toString().padStart(2, "0")}시
-                  </option>
-                );
-              })}
-            </select>
-          </div>
-          <weather-component>
-            {ultraSrtNcst.map((arr, index) => (
-              <WeatherScreen
-                key={index}
-                baseDate={arr.baseDate}
-                baseTime={arr.baseTime}
-                category={arr.category}
-                obsrValue={arr.obsrValue}
+        {fcstLoad ? (
+          <div className="hiddenContent">
+            <div className={styles.timeSelect}>
+              <p>Time</p>
+              {fcstTime}
+              <input
+                type="time"
+                onChange={onChangeFcstTime}
+                value={`${fcstTime.slice(0, 2)}:${fcstTime.slice(2, 4)}`}
               />
-            ))}
-          </weather-component>
-          {/* <div className={styles.timeSelect}>
-            <p>Time</p>
-            <select
-              onChange={onChange}
-              disabled={disabled}
-              value={ultraSrtFcstTime}
-            >
-              {selectTime.map((arr, index) => {
-                return (
-                  <option
-                    key={index}
-                    value={`${arr.toString().padStart(2, "0")}00`}
-                    disabled={dateTime.substring(0, 2) - 1 < index}
-                  >
-                    {arr.toString().padStart(2, "0")}시
-                  </option>
-                );
-              })}
-            </select>
+            </div>
+            <weather-component>
+              {fcst.map((arr, index) => (
+                <WeatherScreen
+                  key={index}
+                  baseDate={arr.baseDate}
+                  baseTime={arr.baseTime}
+                  category={arr.category}
+                  value={arr.fcstValue}
+                />
+              ))}
+            </weather-component>
           </div>
-          <weather-component>
-            {ultraSrtFcst.map((arr, index) => (
-              <WeatherScreen
-                key={index}
-                baseDate={arr.baseDate}
-                baseTime={arr.baseTime}
-                category={arr.category}
-                obsrValue={arr.obsrValue}
-              />
-            ))}
-          </weather-component> */}
-        </center-component>
-      )}
+        ) : null}
+      </center-component>
     </main>
   );
 }
