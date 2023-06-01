9c82141874276c29872ce6a67ae01ad69e65ddea
Hansan529 <hansan0529@gmail.com>
2023-06-01 03:03:08 +0000

    update: 소스코드 경로 추가

--- src/scripts/components/Footer.js
+++ src/scripts/components/Footer.js
@@ -8,6 +8,7 @@ function Footer() {
             <a
               href="https://www.data.go.kr/tcs/dss/selectApiDataDetailView.do?publicDataPk=15084084"
               target="_blank"
+              rel="noreferrer"
             >
               기상청 단기예보
             </a>
@@ -16,8 +17,18 @@ function Footer() {
             <a
               href="https://developers.kakao.com/docs/latest/ko/local/dev-guide#coord-to-address"
               target="_blank"
+              rel="noreferrer"
             >
               카카오 지도 (주소)
+            </a>
+          </small>
+          <small>
+            <a
+              href="https://github.com/Hansan529/react-weather"
+              target="_blank"
+              rel="noreferrer"
+            >
+              소스코드
             </a>
           </small>
         </div>
