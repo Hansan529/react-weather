415079d44cc048d65debcd1795c5293c930a4697
Hansan529 <hansan0529@gmail.com>
2023-06-05 06:00:27 +0000

    update: center 컴포넌트 추가, 사용 브라우저 표시

--- src/scripts/components/Footer.js
+++ src/scripts/components/Footer.js
@@ -3,86 +3,89 @@ import styles from "../../styles/components/Footer.module.css";
 function Footer() {
   return (
     <footer>
-      <div>
-        <h3>사용한 API</h3>
-        <ul className={styles.ul}>
-          <li>
-            <a
-              href="https://www.data.go.kr/tcs/dss/selectApiDataDetailView.do?publicDataPk=15084084"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i>
-                <picture>
-                  <source
-                    srcSet={`${process.env.PUBLIC_URL}/images/components/logo-government.svg`}
-                    media="(min-width: 1200px)"
-                    width="40"
-                    height="40"
-                  />
-                  <img
-                    src={`${process.env.PUBLIC_URL}/images/components/logo-government.svg`}
-                    alt="github"
-                    width="30"
-                    height="30"
-                  />
-                </picture>
-              </i>
-              기상청 단기예보
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://developers.kakao.com/docs/latest/ko/local/dev-guide#coord-to-address"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className={styles.kakaoLogo}>
-                <picture>
-                  <source
-                    srcSet={`${process.env.PUBLIC_URL}/images/components/logo-kakao.svg`}
-                    media="(min-width: 1200px)"
-                    width="40"
-                    height="40"
-                  />
-                  <img
-                    src={`${process.env.PUBLIC_URL}/images/components/logo-kakao.svg`}
-                    alt="github"
-                    width="25"
-                    height="25"
-                  />
-                </picture>
-              </i>
-              카카오 지도 (주소)
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://github.com/Hansan529/react-weather"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i>
-                <picture>
-                  <source
-                    srcSet={`${process.env.PUBLIC_URL}/images/components/logo-github.svg`}
-                    media="(min-width: 1200px)"
-                    width="40"
-                    height="40"
-                  />
-                  <img
-                    src={`${process.env.PUBLIC_URL}/images/components/logo-github.svg`}
-                    alt="github"
-                    width="30"
-                    height="30"
-                  />
-                </picture>
-              </i>
-              소스코드
-            </a>
-          </li>
-        </ul>
-      </div>
+      <center-component>
+        <div>
+          <h3>사용한 API</h3>
+          <ul className={styles.ul}>
+            <li>
+              <a
+                href="https://www.data.go.kr/tcs/dss/selectApiDataDetailView.do?publicDataPk=15084084"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <i>
+                  <picture>
+                    <source
+                      srcSet={`${process.env.PUBLIC_URL}/images/components/logo-government.svg`}
+                      media="(min-width: 1200px)"
+                      width="40"
+                      height="40"
+                    />
+                    <img
+                      src={`${process.env.PUBLIC_URL}/images/components/logo-government.svg`}
+                      alt="github"
+                      width="30"
+                      height="30"
+                    />
+                  </picture>
+                </i>
+                기상청 단기예보
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://developers.kakao.com/docs/latest/ko/local/dev-guide#coord-to-address"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <i className={styles.kakaoLogo}>
+                  <picture>
+                    <source
+                      srcSet={`${process.env.PUBLIC_URL}/images/components/logo-kakao.svg`}
+                      media="(min-width: 1200px)"
+                      width="40"
+                      height="40"
+                    />
+                    <img
+                      src={`${process.env.PUBLIC_URL}/images/components/logo-kakao.svg`}
+                      alt="github"
+                      width="25"
+                      height="25"
+                    />
+                  </picture>
+                </i>
+                카카오 지도 (주소)
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/Hansan529/react-weather"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <i>
+                  <picture>
+                    <source
+                      srcSet={`${process.env.PUBLIC_URL}/images/components/logo-github.svg`}
+                      media="(min-width: 1200px)"
+                      width="40"
+                      height="40"
+                    />
+                    <img
+                      src={`${process.env.PUBLIC_URL}/images/components/logo-github.svg`}
+                      alt="github"
+                      width="30"
+                      height="30"
+                    />
+                  </picture>
+                </i>
+                소스코드
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.userAgent}>{navigator.userAgent}</div>
+      </center-component>
     </footer>
   );
 }
